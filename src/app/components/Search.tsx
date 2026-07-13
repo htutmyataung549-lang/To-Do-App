42f2c35b12c50b9369963ca421253b90bb67541d
Htut Myat Aung <htutmyataung549@gmail.com>
2026-07-13 02:50:16 +0000

    feat: Button Modal

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,9 @@
+import { Input, InputField } from "@/components/ui/input";
+
+export default function SearchBar() {
+  return (
+    <Input>
+      <InputField placeholder="Search Todo..." />
+    </Input>
+  );
+}
